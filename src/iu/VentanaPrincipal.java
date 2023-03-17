f3ddde25b439c5adc4b6d6888436dbff1ab81913
braisd11 <braisdiaz97@gmail.com>
2023-03-17 12:24:34 +0100

    Commit

--- src/iu/VentanaPrincipal.java
+++ src/iu/VentanaPrincipal.java
@@ -1534,24 +1534,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         tqlbtnPausa.setSelected(false);
     }
     //FIN MÉTODOS PARA PAUSAR E QUITAR PAUSA//////////
-    
-    
-    
-    
-    //MÉTODOS PARA O FINAL DO XOGO/////////////
-    
-    //No caso de perder mostra a mensaxe de que se perdeu.
-    public void mostrarFinDoXogo(){
-        timer.stop();
-        tiempo.stop();
-        xogo1.setPausa(true);
-        panelGameOver.setVisible(true);
-        gameOver.setVisible(true);
-        botonGardarPuntuacion.setVisible(true);
-        sound.getSonido().stop();
-        sound.getSonidoGameOver().start();
-    }
-    //FIN DE MÉTODOS PARA O FINAL DO XOGO/////////////
+
     
     private void reiniciarContaAtras(){
         numContaAtras=3;
--- src/modelo/fichas/FichaZEspejo.java
+++ src/modelo/fichas/FichaZEspejo.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package modelo;
+package modelo.fichas;
 
+import modelo.Xogo;
 import static java.awt.Color.ORANGE;
 
 /**
